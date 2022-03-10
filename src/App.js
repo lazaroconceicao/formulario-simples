ab572907426d03b750e43ecb8b1a474d6eba03d6
lazaroconceicao <joselazaroc@gmail.com>
2022-03-10 19:26:52 -0300

    Style container form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
 import FormularioCadstro from "./components/FomularioCadstro/FormularioCadstro";
+import { Container } from "@material-ui/core";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Container component="article" maxWidth="sm">
         <h1>Formulário de Cadastro</h1>
         <FormularioCadstro />
-      </Fragment>
+      </Container>
     );
   }
 }
